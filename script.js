deacfd9098abdd0a6f9c6942f6449e82e6fcb14e
Aby Dzakiyyah <abydzakiyyah@gmail.com>
2019-03-16 11:52:24 +0800

    Update Base

---
+++ script.js
@@ -0,0 +1,78 @@
+$(document).ready(function(){
+    $('.next').on('click', function(){
+        var $this = $(this);
+        var currentImg = $('.active');
+        var nextImg = currentImg.next();
+
+        if(nextImg.length){
+            currentImg.removeClass('active fadeIn animated').css('z-index', -10);
+            nextImg.addClass('active fadeIn animated').css('z-index', 10);
+        }else{
+            nextImg = currentImg.parent().children().first();
+            currentImg.removeClass('active fadeIn animated').css('z-index', -10);
+            nextImg.addClass('active fadeIn animated').css('z-index', 10);
+        }
+
+        if(nextImg.is("video")){
+            nextImg[0].play();
+            nextImg[0].onended = function () {
+                $this.click();
+            }
+        }else{
+            setTimeout(function () {
+                $this.click();
+            },3000)
+        }
+        if(currentImg.is("video")){
+            //stop video sebelumnya kemudian pindah
+            currentImg[0].pause();
+            currentImg[0].currentTime=0;
+        }
+    });
+
+    $('.prev').on('click', function(){
+        var $this = $(this);
+        var currentImg = $('.active');
+        var prevImg = currentImg.prev();
+
+        if(prevImg.length){
+            currentImg.removeClass('active fadeIn animated').css('z-index', -10);
+            prevImg.addClass('active fadeIn animated').css('z-index', 10);
+        }else{
+            prevImg = currentImg.parent().children().last();
+            currentImg.removeClass('active fadeIn animated').css('z-index', -10);
+            prevImg.addClass('active fadeIn animated').css('z-index', 10);
+        }
+
+        if(prevImg.is("video")){
+            prevImg[0].play();
+            prevImg[0].onended = function () {
+                $this.click();
+            }
+        }else{
+            setTimeout(function () {
+                $this.click();
+            },3000)
+        }
+        if(currentImg.is("video")){
+            //stop video sebelumnya kemudian pindah
+            currentImg[0].pause();
+            currentImg[0].currentTime=0;
+        }
+    });
+
+});
+
+window.onload = function() {
+    // var context = new AudioContext();
+    setTimeout(function () {
+        $(".next").click();
+    },"3000");
+};
+// autoplay();
+
+function autoplay(){
+    setTimeout(function () {
+        $(".next").click();
+    },"3000");
+}
